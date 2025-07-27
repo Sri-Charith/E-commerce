620cc1de66912e2dd0710b98610c778e2e224240
Sri Charith Rampelli <charithrampelli2005@gmail.com>
2025-07-28 01:15:51 +0530

    Product page with descriptions

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -3,8 +3,8 @@ import { ShopContext } from "./../context/ShopContextProvider";
 import { useParams } from "react-router-dom";
 import Breadcrum from "../components/Breadcrums/Breadcrum";
 import ProductDisplay from "../components/ProductDisplay/ProductDisplay";
-// import DescriptionBox from "../components/DescriptionBox/DescriptionBox";
-// import RelatedProducts from "../components/RelatedProducts/RelatedProducts";
+import DescriptionBox from "../components/DescriptionBox/DescriptionBox";
+import RelatedProducts from "../components/RelatedProducts/RelatedProducts";
 
 const Product = () => {
   const { all_product } = useContext(ShopContext);
@@ -13,8 +13,8 @@ const Product = () => {
   return <div>
     <Breadcrum product = {product}/>
     <ProductDisplay product={product}/>
-    {/* <DescriptionBox/>
-    <RelatedProducts/> */}
+    <DescriptionBox/>
+    <RelatedProducts/>
   </div>;
 };
 
