99e846ca328156369f0d76c19e51775d2fc0c70d
Sri Charith Rampelli <charithrampelli2005@gmail.com>
2025-08-01 23:20:06 +0530

    admin-page adding product

--- admin/src/Pages/Admin/Admin.jsx
+++ admin/src/Pages/Admin/Admin.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import Sidebar from "../../components/sidebar/sidebar";
- // ✅ go up two levels
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import {Routes, Route} from "react-router-dom";
 
 const Admin = () => {
   return (
     <div>
       <Sidebar/>
+      <Routes>
+        <Route path="/addproduct" element={<AddProduct/>} />
+        <Route path="/listproduct" element={<ListProduct/>} />
+      </Routes>
       </div>
   )
 }
