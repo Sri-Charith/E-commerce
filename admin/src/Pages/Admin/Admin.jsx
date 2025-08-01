493c050f7ec5fd218d19cbf9ecd4abf0cfd30c72
Sri Charith Rampelli <charithrampelli2005@gmail.com>
2025-08-01 22:53:26 +0530

    admin Navbar and Sidebar

---
+++ admin/src/Pages/Admin/Admin.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from "../../components/sidebar/sidebar";
+ // ✅ go up two levels
+
+const Admin = () => {
+  return (
+    <div>
+      <Sidebar/>
+      </div>
+  )
+}
+
+export default Admin
